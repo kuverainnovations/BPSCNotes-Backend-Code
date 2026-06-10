2d86ff20b4ae2f80e801d52dc879a37196267ab5
Suresh <sureshvipparla523@gmail.com>
2026-06-10 07:33:53 +0530

    same

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { CacheModule } from '@nestjs/cache-manager';
+import { ScheduleModule } from '@nestjs/schedule';
 import { APP_GUARD, APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { join } from 'path';
 import { redisStore } from 'cache-manager-ioredis-yet';
@@ -108,6 +109,7 @@ import { MarketplaceModule }   from './modules/marketplace.module';
     }),
 
     // ── Feature Modules ───────────────────────────────────────
+    ScheduleModule.forRoot(),
     AuthModule,
     AdminModule,
     CoursesModule,
